8010dfb814a698cd943bf9c11c89b98f4ef81905
Andrei Pistirica <andrei.pistirica@microchip.com>
2015-11-20 19:09:54 +0530

    drivers: mmc: add driver for Microchip PIC32 SDHCI controller.
    PIC32 architecture has in-built SDHCI controller. This driver implements
    platform specific glue and fixups to use generic sdhci functionalities.
    
    Signed-off-by: Andrei Pistirica <andrei.pistirica@microchip.com>
    Signed-off-by: Sandeep Sheriker Mallikarjun <sandeepsheriker.mallikarjun@microchip.com>
    Signed-off-by: Purna Chandra Mandal <purna.mandal@microchip.com>
    
    Series-changes: 2
    - drop sdhci shared bus configuration (for shared interrupt, clock pins)

---
+++ drivers/mmc/pic32_sdhci.c
@@ -0,0 +1,63 @@
+/*
+ * Support of SDHCI for Microchip PIC32 SoC.
+ *
+ * Copyright (C) 2015 Microchip Technology Inc.
+ * Andrei Pistirica <andrei.pistirica@microchip.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <dm.h>
+#include <common.h>
+#include <sdhci.h>
+#include <asm/errno.h>
+#include <mach/pic32.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+static int pic32_sdhci_probe(struct udevice *dev)
+{
+	struct sdhci_host *host = dev_get_priv(dev);
+	u32 f_min_max[2];
+	int ret;
+
+	ret = fdtdec_get_int_array(gd->fdt_blob, dev->of_offset,
+				   "clock-freq-min-max", f_min_max, 2);
+	if (ret) {
+		printf("sdhci: clock-freq-min-max not found\n");
+		return ret;
+	}
+
+	return add_sdhci(host, f_min_max[1], f_min_max[0]);
+}
+
+static int pic32_sdhci_ofdata_to_platdata(struct udevice *dev)
+{
+	struct sdhci_host *host = dev_get_priv(dev);
+	fdt_addr_t addr;
+
+	addr = dev_get_addr(dev);
+	if (addr == FDT_ADDR_T_NONE)
+		return -EINVAL;
+
+	host->name	= (char *)dev->name;
+	host->ioaddr	= pic32_ioremap(addr);
+	host->quirks	= SDHCI_QUIRK_NO_HISPD_BIT;
+	host->bus_width	= fdtdec_get_int(gd->fdt_blob, dev->of_offset,
+					"bus-width", 4);
+	return 0;
+}
+
+static const struct udevice_id pic32_sdhci_ids[] = {
+	{ .compatible = "microchip,pic32mzda-sdhci" },
+	{ }
+};
+
+U_BOOT_DRIVER(pic32_sdhci_drv) = {
+	.name			= "pic32_sdhci",
+	.id			= UCLASS_MMC,
+	.of_match		= pic32_sdhci_ids,
+	.probe			= pic32_sdhci_probe,
+	.ofdata_to_platdata	= pic32_sdhci_ofdata_to_platdata,
+	.priv_auto_alloc_size	= sizeof(struct sdhci_host),
+};
